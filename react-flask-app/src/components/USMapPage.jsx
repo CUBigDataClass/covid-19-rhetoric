28acd0a9feceef6674a4addf275da5a069504470
Riad Shash <riadshash@yahoo.com>
2020-04-16 22:45:21 -0600

    Added US clickable Map that fetches state data
    Co-authored-by: vandana28

---
+++ react-flask-app/src/components/USMapPage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import TopNavBar from "./TopNavBar"
+import TweetCard from "./TweetCard"
+import '../CSS/App.css';
+import USAMap from "react-usa-map";
+
+
+export default class USMapPage extends React.Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = { tweetIds: [] }
+    }
+
+
+    mapHandler = (event) => {
+        //alert(event.target.dataset.name);
+        var state_name = event.target.dataset.name
+        var vm = this
+        fetch('/GetTopPostsState/' + state_name).then(res => res.json()).then(data => {
+            vm.setState({
+                tweetIds: data
+            })
+        })
+    };
+
+
+    createTwitterCard(tweetId) {
+        console.log(tweetId)
+        return (
+            <TweetCard
+                key={tweetId}
+                tweetId={tweetId}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <TopNavBar />
+                <USAMap onClick={this.mapHandler} />
+                <dl className="dictionary">{this.state.tweetIds.map(this.createTwitterCard)}</dl>
+            </div>
+        );
+    }
+}
--- react-flask-app/src/index.js
+++ react-flask-app/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css';
 //import App from './App';
 import App from "./components/App";
+import USMapPage from "./components/USMapPage"
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<USMapPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
